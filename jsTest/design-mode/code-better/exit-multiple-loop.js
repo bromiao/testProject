2606bc5430c7b251406bff793f5b1402a418c0ba
bromiao <ss@gmail.com>
2024-04-23 11:25:42 +0800

    判断是否为循环引用对象

--- jsTest/design-mode/code-better/exit-multiple-loop.js
+++ jsTest/design-mode/code-better/exit-multiple-loop.js
@@ -1,13 +1,16 @@
 /************使用return退出多重循环************/
 
-var print = function( i ){
-  console.log( i );
+var print = function (...args) {
+  console.log(...args);
 };
-var func = function(){
-  for ( var i = 0; i < 10; i++ ){
-    for ( var j = 0; j < 10; j++ ){
-      if ( i * j >30 ){
-        return print( i );
+var func = function () {
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      for (var k = 0; k < 10; k++) {
+        if (k === 5) {
+          return print(i, j, k);
+        }
+        console.log(1111, i, j, k);
       }
     }
   }
